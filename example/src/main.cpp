857c07e39ffc99fe7c0639a8d2e6e8783b3d5f74
Elysia <c.elysia@foxmail.com>
2024-10-09 21:44:16 +0800

    Fix LingmoUI source find

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,9 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <qlogging.h>
 #include <qtenvironmentvariables.h>
 
 int main(int argc, char* argv[])
 {
+    qDebug() << "LINGMOUI_TEMP_IMPORT_PATH: " << LINGMOUI_TEMP_IMPORT_PATH;
     QString path = QString(LINGMOUI_TEMP_IMPORT_PATH) + ":" + qgetenv("QML_IMPORT_PATH");
     qputenv("QML_IMPORT_PATH", path.toStdString().c_str());
 
