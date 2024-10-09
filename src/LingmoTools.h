70e20d99277a197fd80fef49d2f5c23817e3e3f3
Elysia <c.elysia@foxmail.com>
2024-10-09 20:23:04 +0800

    Added a method to query wallpaper for lingmo

--- src/LingmoTools.h
+++ src/LingmoTools.h
@@ -1,6 +1,8 @@
 #ifndef LINGMOTOOLS_H
 #define LINGMOTOOLS_H
 
+#include <cstdint>
+
 #include <QIcon>
 #include <QImage>
 #include <QObject>
@@ -8,6 +10,20 @@
 #include <QQuickWindow>
 
 #include "singleton.h"
+
+typedef std::uint64_t hash_t;
+
+constexpr hash_t prime = 0x100000001B3ull;
+constexpr hash_t basis = 0xCBF29CE484222325ull;
+
+// Compute the hash of a string.
+hash_t hash_(char const* str);
+
+constexpr hash_t hash_compile_time(char const* str, hash_t last_value = basis)
+{
+    return *str ? hash_compile_time(str + 1, (*str ^ last_value) * prime)
+                : last_value;
+}
 
 /**
  * @brief The LingmoTools class. Contains some small utils.
