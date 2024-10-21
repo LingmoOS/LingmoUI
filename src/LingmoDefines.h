8059c088151f296e4762edd0cf3bea699186aaf9
Elysia <c.elysia@foxmail.com>
2024-10-21 15:46:29 +0800

    Added LingmoTheme

--- src/LingmoDefines.h
+++ src/LingmoDefines.h
@@ -8,6 +8,9 @@
 #include <QObject>
 #include <QQmlEngine>
 
+/**
+ * @brief LingmoWindowType
+ */
 namespace LingmoWindowType {
 Q_NAMESPACE
 
@@ -19,6 +22,19 @@ enum LaunchMode {
 Q_ENUM_NS(LaunchMode)
 
 QML_NAMED_ELEMENT(LingmoWindowType)
+}
+
+namespace LingmoThemeType {
+Q_NAMESPACE
+enum DarkMode {
+    System = 0x0000,
+    Light = 0x0001,
+    Dark = 0x0002,
+};
+
+Q_ENUM_NS(DarkMode)
+
+QML_NAMED_ELEMENT(LingmoThemeType)
 }
 
 #endif // LINGMODEFINES_H
