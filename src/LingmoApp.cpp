d6035f295b8b940719daa28e0c1cef644db4b643
Elysia <c.elysia@foxmail.com>
2024-11-23 21:33:29 +0800

    Fix build problem

--- src/LingmoApp.cpp
+++ src/LingmoApp.cpp
@@ -2,6 +2,18 @@
 
 #include "LingmoApp.h"
 
+#include <QGuiApplication>
+#include <QQuickItem>
+#include <QTimer>
+#include <QUuid>
+#include <QFontDatabase>
+#include <QClipboard>
+#include <QTranslator>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <utility>
+#include "LingmoIconDef.h"
+
 LingmoApp::LingmoApp(QObject* parent)
     : QObject { parent }
 {
@@ -27,3 +39,21 @@ void LingmoApp::init(QObject* launcher, QLocale locale)
         }
     }
 }
+
+[[maybe_unused]] QJsonArray LingmoApp::iconData(const QString &keyword) {
+    QJsonArray arr;
+    QMetaEnum enumType = LingmoIcons::staticMetaObject.enumerator(
+        LingmoIcons::staticMetaObject.indexOfEnumerator("Type"));
+    for (int i = 0; i <= enumType.keyCount() - 1; ++i) {
+        QString name = enumType.key(i);
+        int icon = enumType.value(i);
+        if (keyword.isEmpty() || name.contains(keyword)) {
+            QJsonObject obj;
+            obj.insert("name", name);
+            obj.insert("icon", icon);
+            arr.append(obj);
+        }
+    }
+    return arr;
+}
+
