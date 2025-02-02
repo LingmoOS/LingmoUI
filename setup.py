5016664f47c605f176e4fc7195f3720d48b381c2
elysia-best <39023210+elysia-best@users.noreply.github.com>
2025-02-02 10:09:35 +0800

    build: update version to 3.0.0b1

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class BuildExt(build_ext):
 lingmoui = CMakeExtension("LingmoUI")
 
 setup(name="LingmoUIPy",
-      version="3.0",
+      version="3.0.0b1",
       description="This is LingmoUI for Python",
       ext_modules=[lingmoui],  # mymath 现在是 CMakeExtension 类的实例了
       packages=['LingmoUIPy'],
