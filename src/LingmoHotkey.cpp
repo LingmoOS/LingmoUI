f27b11c4a5b0249348dd946732644c188f59ff57
elysia-best <39023210+elysia-best@users.noreply.github.com>
2025-02-11 11:13:14 +0800

    fix(LingmoHotkey): properly disconnect and delete hotkey on sequence change

--- src/LingmoHotkey.cpp
+++ src/LingmoHotkey.cpp
@@ -9,6 +9,9 @@ LingmoHotkey::LingmoHotkey(QObject* parent)
     _isRegistered = false;
     connect(this, &LingmoHotkey::sequenceChanged, this, [=] {
         if (_hotkey) {
+            QObject::disconnect(_hotkey.get(), &QHotkey::activated, qApp, nullptr);
+            QObject::disconnect(_hotkey.get(), &QHotkey::registeredChanged, qApp, nullptr);
+            _hotkey->deleteLater();
             _hotkey.reset();
         }
         _hotkey = std::make_shared<QHotkey>(QKeySequence(_sequence), true, qApp);
